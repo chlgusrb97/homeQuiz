01f818ba3d28129bab6eb71844c3df0a6129124c
chlgusrb97 <ikolp34@gmail.com>
2023-09-17 18:30:33 +0900

    layout header

---
+++ svelteKit-app/src/components/Header/Header.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import "./header.css"
+  
+  type Languages = {
+    name: string
+  }
+  
+  const languages: Languages[] = [{ name: 'English' }, { name: '한국어' }];
+
+  let language = languages[0].name
+  let isToggle = false
+
+  const selectToggle = () => {
+    isToggle = !isToggle
+  }
+
+  const hadleSelectedLanguage = (selectedLanguage: string) => {
+    language = selectedLanguage
+    isToggle = false
+  }
+
+  const handleClickCallWaiter = () => {
+    alert("Waiter Called")
+  }
+
+</script>
+
+<header id="header">
+  <ul id="header-menu">
+    <li class="header-menu-language">
+      <button on:click={() => selectToggle()}>
+        <p>{language}</p>
+        <img src="/chevron-down.png" alt="chevron-icon" style={`transform: ${isToggle ? "rotate(180deg)" : "rotate(0)"}`}>
+      </button>
+
+      {#if isToggle}
+        <div class="header-menu-language-list">
+          {#each languages as selectedLanguage}
+            <button on:click={() => hadleSelectedLanguage(selectedLanguage.name)}>
+              {selectedLanguage.name}
+            </button>
+          {/each}
+        </div>
+      {/if}
+    </li>
+
+    <li class="header-menu-call" >
+      <button on:click={() => handleClickCallWaiter()}>
+        <img src="/notifications_active.png" alt="notifications-icon" />
+        <p>Call Waiter</p>
+      </button>
+    </li>
+
+    <li class="header-menu-bill" >
+      <img src="/receipt.png" alt="receipt-icon">
+      <p>View Bill</p>
+    </li>
+
+    <li class="header-menu-shopping" >
+      <img src="shopping_cart.png" alt="shopping-cart-icon">
+      <p>My Cart</p>
+      <div class="menu-shopping-count">0</div>
+    </li>
+
+    <li class="header-menu-table" >
+      <img src="/table.png" alt="table-icon" />
+      <p class="menu-table-number">12</p>
+    </li>
+  </ul>
+</header>
---
+++ svelteKit-app/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Header from "../components/Header/Header.svelte"
+  import "../styles/reset.css"
+</script>
+
+<Header />
+
+<slot></slot>
